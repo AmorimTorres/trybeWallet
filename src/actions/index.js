d3c6622b31cc07d11df29bda0968186f659036a9
André <amorimtorresandre@gmail.com>
2022-03-04 17:09:12 -0300

    requisito 1 feito - VQV

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+export const SUBMIT_LOGIN_DATA = 'SUBMIT_LOGIN_DATA';
+
+const loginData = (payload) => ({
+  type: SUBMIT_LOGIN_DATA,
+  payload,
+});
+
+export default loginData;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,87 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import loginData from '../actions/index';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  isDisabled = () => {
+    const PASSWORD_LENGTH = 6;
+    const regex = /^\S+@\S+\.\S+$/;
+    const { email, password } = this.state;
+    return (
+      email !== ''
+      && regex.test(email)
+      && password.length >= PASSWORD_LENGTH
+    );
+  }
+
+  handleClick = () => {
+    const { email } = this.state;
+    const { sendEmail, history } = this.props;
+    sendEmail(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <fieldset>
+        <label htmlFor="email">
+          {' '}
+          Email
+          <input
+            data-testid="email-input"
+            name="email"
+            type="text"
+            onChange={ this.handleChange }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="password">
+          {' '}
+          Password
+          <input
+            data-testid="password-input"
+            name="password"
+            type="password"
+            onChange={ this.handleChange }
+            value={ password }
+          />
+        </label>
+        <button
+          type="button"
+          label="Entrar"
+          disabled={ !this.isDisabled() }
+          onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
+      </fieldset>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  sendEmail: (email) => dispatch(loginData(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
+
+Login.propTypes = {
+  sendEmail: PropTypes.func.isRequired,
+  history: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
